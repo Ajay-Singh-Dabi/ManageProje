0628cde1bfc04e501134be701813e6fcae750706
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-12 00:54:31 +0530

    +FireStore Class

---
+++ app/src/main/java/com/example/managproje/firebase/FireStoreClass.kt
@@ -0,0 +1,14 @@
+package com.example.managproje.firebase
+
+import com.example.managproje.activities.SignUpActivity
+import com.example.managproje.models.User
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FireStoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+    fun registerUser(activity: SignUpActivity, userInfo: User){
+
+    }
+}

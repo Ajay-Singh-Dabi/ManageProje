788bd8ec8428daf33897e65c04493383f9107872
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-11 22:49:51 +0530

    +onBackPressed +Removed the bar

--- app/src/main/java/com/example/managproje/SignUpActivity.kt
+++ app/src/main/java/com/example/managproje/SignUpActivity.kt
@@ -2,12 +2,18 @@ package com.example.managproje
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         setupActionBar()
     }
@@ -20,5 +26,10 @@ class SignUpActivity : AppCompatActivity() {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
+
+        toolbar_sign_up_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
+
 }

de772fe28757d2b0703d50a763ae2e292042597c
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-12 00:02:03 +0530

    +registerUser(TODO:Firebase link)

--- app/src/main/java/com/example/managproje/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/managproje/activities/SignUpActivity.kt
@@ -2,11 +2,13 @@ package com.example.managproje.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.managproje.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -30,6 +32,42 @@ class SignUpActivity : AppCompatActivity() {
 
         toolbar_sign_up_activity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        btn_sign_up.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser(){
+        val name: String = et_name.text.toString().trim{it <= ' '}
+        val email: String = et_email.text.toString().trim{it <= ' '}
+        val password: String = et_password.text.toString().trim{it <= ' '}
+
+        if (validateForm(name, email, password)){
+            Toast.makeText(this@SignUpActivity,
+                "Now we can register a new user",
+                Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean{
+        return when{
+            TextUtils.isEmpty(name)->{
+                showErrorSnackBar("Please Enter a Name")
+                false
+            }
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please Enter an Email")
+                false
+            }
+            TextUtils.isEmpty(password)->{
+                showErrorSnackBar("Please Enter the Password")
+                false
+            }else ->{
+                true
+            }
         }
     }
 

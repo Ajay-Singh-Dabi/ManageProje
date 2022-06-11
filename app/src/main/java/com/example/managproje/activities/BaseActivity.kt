39aa58cce8906ead17ec502db2a6c71eb94b6346
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-11 23:48:20 +0530

    BaseActivity fun and SnackBar prepared

--- app/src/main/java/com/example/managproje/activities/BaseActivity.kt
+++ app/src/main/java/com/example/managproje/activities/BaseActivity.kt
@@ -3,12 +3,18 @@ package com.example.managproje.activities
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.managproje.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.dialog_progress.*
+
 
 open class BaseActivity : AppCompatActivity() {
 
-    private val doubleBackToExitPressedOnce = false
+    private var doubleBackToExitPressedOnce = false
 
     private lateinit var mProgressDialog: Dialog
 
@@ -23,5 +29,39 @@ open class BaseActivity : AppCompatActivity() {
         mProgressDialog.tv_progress_text.text = text
 
         mProgressDialog.show()
+    }
+
+    fun hideProgressDialog(){
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserID(): String{
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit(){
+        if(doubleBackToExitPressedOnce){
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this,
+            R.string.please_click_back_again_to_exit,
+            Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({
+            doubleBackToExitPressedOnce = false
+        },2000)
+    }
+
+    fun showErrorSnackBar(message: String){
+        val snackbar = Snackbar.make(findViewById(android.R.id.content),
+            message, Snackbar.LENGTH_LONG)
+
+        val snackBarView = snackbar.view
+        snackBarView.setBackgroundColor(ContextCompat
+            .getColor(this,R.color.snackbar_error_color))
+
+        snackbar.show()
     }
 }

e9c346832138db589e7af0f2c6d310e87a54fe78
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-12 18:04:41 +0530

    my profile

--- app/src/main/java/com/example/managproje/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/managproje/activities/MyProfileActivity.kt
@@ -3,10 +3,25 @@ package com.example.managproje.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.managproje.R
+import kotlinx.android.synthetic.main.activity_my_profile.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MyProfileActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+
+        setupActionBar()
+    }
+
+
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_my_profile_activity)
+
+        val actionBar = supportActionBar
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
     }
 }

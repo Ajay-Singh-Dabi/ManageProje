dac55a1fced0813489dfd2ed2279c1a9fb512d1e
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-17 00:27:26 +0530

    Small Bug tasklist->taskList

--- app/src/main/java/com/example/managproje/utils/Constants.kt
+++ app/src/main/java/com/example/managproje/utils/Constants.kt
@@ -21,7 +21,7 @@ object Constants {
     const val PICK_IMAGE_REQUEST_CODE = 2
 
     const val DOCUMENT_ID: String = "documentId"
-    const val TASK_LIST: String = "tasklist"
+    const val TASK_LIST: String = "taskList"
 
     fun showImageChooser(activity: Activity){
         var galleryIntent = Intent(

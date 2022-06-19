2e0d9041f1c7c31a868c4f2217c80a6952ceaa5a
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-20 00:41:00 +0530

    Color dialog

--- app/src/main/java/com/example/managproje/models/Card.kt
+++ app/src/main/java/com/example/managproje/models/Card.kt
@@ -6,13 +6,15 @@ import android.os.Parcelable
 data class Card (
     val name: String = "",
     val createdBy: String = "",
-    val assignedTo: ArrayList<String> = ArrayList()
+    val assignedTo: ArrayList<String> = ArrayList(),
+    val labelColor: String = ""
 
 ): Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString()!!,
         parcel.readString()!!,
-        parcel.createStringArrayList()!!
+        parcel.createStringArrayList()!!,
+        parcel.readString()!!
     ) {
     }
 
@@ -20,6 +22,7 @@ data class Card (
         parcel.writeString(name)
         parcel.writeString(createdBy)
         parcel.writeStringList(assignedTo)
+        parcel.writeString(labelColor)
     }
 
     override fun describeContents(): Int {

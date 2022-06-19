544fc04db7f856dd672fa2bfaf2f19a267ce63f5
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-19 15:04:00 +0530

    Getting Card Name on Title

--- app/src/main/java/com/example/managproje/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/managproje/activities/CardDetailsActivity.kt
@@ -3,14 +3,22 @@ package com.example.managproje.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.managproje.R
+import com.example.managproje.models.Board
+import com.example.managproje.utils.Constants
 import kotlinx.android.synthetic.main.activity_card_details.*
 import kotlinx.android.synthetic.main.activity_members.*
 
 class CardDetailsActivity : AppCompatActivity() {
+
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
 
+        getIntentData()
         setupActionBar()
     }
 
@@ -21,7 +29,22 @@ class CardDetailsActivity : AppCompatActivity() {
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails
+                .taskList[mTaskListPosition]
+                .cards[mCardPosition].name
         }
         toolbar_card_details_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun getIntentData(){
+        if(intent.hasExtra(Constants.BOARD_DETAIL)){
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if(intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)){
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION,-1)
+        }
+        if(intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)){
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION,-1)
+        }
     }
 }

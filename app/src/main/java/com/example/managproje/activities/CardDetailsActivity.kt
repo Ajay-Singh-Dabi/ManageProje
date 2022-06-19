ac31fa9035c001c3416f137770fcaf021414ecbd
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-19 14:54:05 +0530

    setupActionBar as usual

--- app/src/main/java/com/example/managproje/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/managproje/activities/CardDetailsActivity.kt
@@ -3,10 +3,26 @@ package com.example.managproje.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.managproje.R
+import kotlinx.android.synthetic.main.activity_card_details.*
+import kotlinx.android.synthetic.main.activity_members.*
 
 class CardDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
+
+        setupActionBar()
+    }
+
+
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_card_details_activity)
+
+        val actionBar = supportActionBar
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+        toolbar_card_details_activity.setNavigationOnClickListener { onBackPressed() }
     }
 }
